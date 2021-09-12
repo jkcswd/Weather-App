60e584110d3129f50811230d3821a98a916a5328
James Spencer <jameskcs1254@gmail.com>
2021-09-12 13:05:13 +0100

    added error handling to weatherDataProcess

--- src/app.js
+++ src/app.js
@@ -1,43 +1,55 @@
 import apiKey from "./secrets";
 
-const fetchWeatherData = async city => {
+const fetchWeatherData = async city => {//works
   try {
     const response = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`, 
       { mode: 'cors' }
       );
     const dataPromise =  await response.json();
-    
+
+    console.log(dataPromise);
     return dataPromise
   } catch (err) {
     console.log(err);
   }
 }
 
-const weatherDataProcess =  async data => {
+const weatherDataProcess =  async data => { //works
+  try {
+    const weatherData = await data;
+    let weatherObject = {
+      description: weatherData.weather[0].description,
+      temp: weatherData.main.temp,
+      feelTemp: weatherData.main.feels_like,
+      maxTemp: weatherData.main.temp_max,
+      minTemp: weatherData.main.temp_min,
+      pressure: weatherData.main.pressure,
+      humidity: weatherData.main.humidity,
+      windSpeed: weatherData.wind.speed
+    };
+    
+    console.log(weatherObject);
+    return weatherObject
 
-  const weatherData = await data;
-
-  let weatherObject = {
-    description: weatherData.weather[0].description,
-    temp: weatherData.main.temp,
-    feelTemp: weatherData.main.feels_like,
-    maxTemp: weatherData.main.temp_max,
-    minTemp: weatherData.main.temp_min,
-    pressure: weatherData.main.pressure,
-    humidity: weatherData.main.humidity,
-    windSpeed: weatherData.wind.speed
-  };
-  return weatherObject
+  } catch (err) {
+    console.log(err)
+  }
+  
 }
 
-const fetchUserWeather = () => {
-  const button = document.querySelector('button')
-  const input = document.querySelector('input')
+const userWeatherListener = () => { //does not work
+  const button = document.querySelector('button');
 
-  button.addEventListener('click', () => {
-    weatherDataProcess(fetchWeatherData(input.value));
+  button.addEventListener('click', async () => {
+    const input = document.querySelector('input');
+    console.log(input.value);
+    const apiData = await fetchWeatherData(input.value);
+    const processedData = await weatherDataProcess(apiData);
 
+    console.log(processedData);
   })
 }
-weatherDataProcess(fetchWeatherData('london'));
+
+weatherDataProcess(fetchWeatherData('london')); //works
+userWeatherListener(); //does not work?How?
