a52a0a37c45e8b2320178ba6ae6781a6364e5652
James Spencer <jameskcs1254@gmail.com>
2021-10-07 18:21:38 +0100

    Add iconLookup table. Add icon to render in renderDisplay
    I created an object which can be used as a lookup table for the data returned
    from the API call to match it with an icon to render to the display.

--- src/dom.js
+++ src/dom.js
@@ -1,11 +1,30 @@
 import { fetchWeatherData, weatherDataProcess } from "./data";
 
+const iconLookupTable = {
+  "Clouds" : "wi-cloud",
+  "Clear": "wi-day-sunny",
+  "Snow": "wi-snow",
+  "Rain": "wi-rain",
+  "Drizzle": "wi-showers",
+  "Thunderstorm": "wi-thunderstorm",
+  "Mist": "wi-fog",
+  "Smoke": "wi-smoke",
+  "Haze": "wi-day-haze",
+  "Dust": "wi-dust",
+  "Fog": "wi-fog",
+  "Sand": "wi-dust",
+  "Ash": "wi-dust",
+  "Squall": "wi-strong-wind",
+  "Tornado": "wi-tornado"
+}
+
 const renderDisplay = (processedData) => {
   const display = document.querySelector('.display');
   const displayDiv = document.createElement('div');
 
   display.innerHTML = '';
   displayDiv.innerHTML = `
+  <i class="wi ${iconLookupTable[processedData.main]}" style="font-size:100px"></i>
   <p>Weather: ${processedData.description}<p/>
   <p>Temperature: ${processedData.temp}<p/>
   <p>Feels like: ${processedData.feelTemp}<p/>
